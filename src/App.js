f10968d13d9d47e15846f3f588d66d517509b3a3
Udit Kumar Nandy <uditkumarnandy312@gmail.com>
2025-02-16 02:05:12 +0530

    Updated the nav bar in app component

--- src/App.js
+++ src/App.js
@@ -5,36 +5,50 @@ import { Hooks } from "./components/hooks/Hooks";
 import { InternaclConcepts } from "./components/internal-concepts/InternalConcepts";
 import Networking from "./components/system-design/networking";
 import Communication from "./components/system-design/communication";
+import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
 
 function App() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div className="navbar">
-        <Link className="link" to="/fundamental">
-          Fundamentals
-        </Link>
-        <Link className="link" to="/hooks">
-          React Hooks
-        </Link>
-        <Link className="link" to="/internal-concepts">
-          Internal Concepts
-        </Link>
-        <Link className="link" to="/networking">
-          Networking
-        </Link>
-        <Link className="link" to="/communication">
-          Communication
-        </Link>
+    <div className="view-page">
+      <Sidebar
+        rootStyles={{
+          background: "#121417",
+          height: "100vh",
+          color: "#121417",
+          "font-size": "16px",
+          "font-weight": "500",
+          "box-shadow": "1px 0 16px rgba(83, 80, 80, 0.6)",
+        }}
+      >
+        <Menu>
+          <MenuItem component={<Link to="/fundamental" />}>
+            Fundamentals
+          </MenuItem>
+          <MenuItem component={<Link to="/hooks" />}> React Hooks </MenuItem>
+          <MenuItem component={<Link to="/internal-concepts" />}>
+            Internal Concepts{" "}
+          </MenuItem>
+          <SubMenu label="System Design">
+            <MenuItem component={<Link to="/networking" />}>
+              Networking
+            </MenuItem>
+            <MenuItem component={<Link to="/communication" />}>
+              Communication
+            </MenuItem>
+          </SubMenu>
+        </Menu>
+      </Sidebar>
+      <div className="content-section">
+        <Routes>
+          <Route path="fundamental" element={<Fundamentals />} />
+          <Route path="hooks" element={<Hooks />} />
+          <Route path="internal-concepts" element={<InternaclConcepts />} />
+          <Route path="networking" element={<Networking />} />
+          <Route path="communication" element={<Communication />} />
+        </Routes>
       </div>
-      <Routes>
-        <Route path="fundamental" element={<Fundamentals />} />
-        <Route path="hooks" element={<Hooks />} />
-        <Route path="internal-concepts" element={<InternaclConcepts />} />
-        <Route path="networking" element={<Networking />} />
-        <Route path="communication" element={<Communication />} />
-      </Routes>
     </div>
   );
 }
