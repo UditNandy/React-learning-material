a6ff0297a272ace880486456a43b02397dea2cf2
Udit Kumar Nandy <uditkumarnandy312@gmail.com>
2025-02-16 01:11:27 +0530

    Added web sockets

--- src/components/system-design/communication.js
+++ src/components/system-design/communication.js
@@ -59,6 +59,111 @@ const Communication = () => {
           </li>
         </ul>
       </div>
+      <div className="wrapper">
+        <h3>Long Polling</h3>
+        <ul>
+          <li>
+            This is similar to short polling but here you make the request and
+            the server responds only when there is an updated data, or if it
+            takes long time to update data you can get a timeout error.
+          </li>
+          <li>
+            Here the connection is long lived as the server holds the request
+            till there is any update in data/db or timeout.
+          </li>
+          <li>
+            Here the number of api calls that we were making durring short
+            polling reduces.
+          </li>
+          <li>
+            The issue with this type of connection is when the user base is
+            large then there will be enough load on the server as the connection
+            time for each request is quite long.
+          </li>
+          <li>
+            Here the changes has to be made in server in order to support long
+            polling.
+          </li>
+          <li>
+            In many cases we might send some id from client side which will be
+            used by the server to compare and give the updated response.
+          </li>
+          <li>
+            In this case we might make multiple api calls but only if we dont
+            get the desired response from the previous api call.
+          </li>
+          <li>
+            <div>
+              <strong>Examples</strong>
+              <ul>
+                <li>Payment success screen</li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="wrapper">
+        <h3>Web Socket</h3>
+        <ul>
+          <li>
+            It works with full duplex communication. Like the client dont need
+            to send a request to the server to get the response.
+          </li>
+          <li>
+            The connection has to be established in the begining and once the
+            connection is established the client and server can communicate with
+            each other.
+          </li>
+        </ul>
+
+        <h4>Steps to establish a socket connection</h4>
+        <ul>
+          <li>
+            <strong>Handshake</strong> happens from the client where the client
+            sends a HTTP upgrade request
+          </li>
+          <li>HTTP will be upgraded to ws and the connection will get open.</li>
+          <li>
+            <div>Challanges</div>
+            <ul>
+              <li>
+                It is going to use lot of resource based on the number of
+                connections as all the connections are long lived connections.
+              </li>
+              <li>Connetion limits.</li>
+              <li>
+                Sticky sessions : how these things will work with load
+                balancers. Like if a user is connected to server1 the always the
+                user must be redirected to that server.
+              </li>
+              <li>We need to focus on authentication.</li>
+              <li>Firewall/proxy.</li>
+              <li>Vertical/Horizontal Scaling.</li>
+              <li>Testing for real time connections.</li>
+              <li>
+                Backward compactibility : Suppose the web socket is down for
+                some time in such cases the protocol has to be downgraded and we
+                have to use short or long polling.
+              </li>
+              <li>
+                Connections which are expired has to be cleaned or else we will
+                have to keep on scaling the server for new connections.
+              </li>
+            </ul>
+          </li>
+          <li>
+            <div>
+              <strong>Examples</strong>
+              <ul>
+                <li>Analytics Dashboards</li>
+                <li>Trading Dashboards</li>
+                <li>Chart rooms</li>
+                <li>Real Time Collab (google docs,sheets)</li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
